86836ef621f1ca8d8e0077758f12b3144731ef43
Dim Kaff <154969259+dkaffes@users.noreply.github.com>
2025-02-25 14:48:48 +0200

    small improvements

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const shareButton = document.querySelector(".share-btn");
 
 shareButton.addEventListener("click", () => {
   shareContentWrap.classList.toggle("hidden");
-  shareContentWrap.classList.toggle("flex");
-  shareContentWrap.classList.toggle("share-content-wrapper-bg");
+  shareContentWrap.classList.toggle("open");
   shareButton.classList.toggle("share-btn-bg");
 
   // accessibility improvement

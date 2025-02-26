05ac87b152da01b576737140d1ba009fd1a58bb7
Dim Kaff <154969259+dkaffes@users.noreply.github.com>
2025-02-26 12:34:04 +0200

    share button states

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const shareContentWrap = document.querySelector(".share-content-wrapper");
 const shareButton = document.querySelector(".share-btn");
+const shareButtonImg = document.querySelector(".share-btn-img");
 
 shareButton.addEventListener("click", () => {
   shareContentWrap.classList.toggle("hidden");
   shareContentWrap.classList.toggle("open");
-  shareButton.classList.toggle("share-btn-bg");
+  shareButton.classList.toggle("share-btn-active");
+  shareButtonImg.classList.toggle("share-btn-img-active");
 
   // accessibility improvement
   const isExpanded = shareButton.getAttribute("aria-expanded") === "true";
